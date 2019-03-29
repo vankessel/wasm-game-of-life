362f6bb6aafd74733ed7252a0d41e3cdc2903c7c
Kevin Van Kessel <10148659+vankessel@users.noreply.github.com>
2019-03-28 19:05:33 -0700

    Init commit

---
+++ www/index.js
@@ -0,0 +1,59 @@
+// This file has been modified from the original Work.
+
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+
+const CELL_SIZE = 5;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+// Construct the universe, and get its width and height.
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.style.background = GRID_COLOR;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+  drawCells();
+  universe.tick();
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+}
